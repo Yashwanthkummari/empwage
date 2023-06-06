608025c5bd656cefa93615c346e003853c527dcc
“Enter <yashwanth.k1119@gmail.com>
2023-06-06 14:07:48 +0530

    [yashwanth] add: checking for an employee presence

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to emoployee wage Computation"
 
+randomcheck=$((RANDOM%2))
+if [$randomcheck -eq 1 ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
