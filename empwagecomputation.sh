564c07c0ee88a134483aa4585f4529e598553166
“Enter <yashwanth.k1119@gmail.com>
2023-06-06 18:06:07 +0530

    [yashwanth] Refactor: checking totalworking hrs

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,23 +5,28 @@ Wageperhour=20
 Parttime=2
 Fulltime=1
 numworkingdays=20
-Totalsalary=0
-for (( day=1; day<=numworkingdays; day++ ))
-do
-randomcheck=$((RANDOM%3))
-case $randomcheck in
-	$Fulltime)
-	echo "employee is present"
-	Workinghours=8;;
+maxhrsinamonth=100
+Totalemphrs=0
+totalworkingdays=0
 
-	$Parttime) 
-	echo "employee is Parttime"
-	Workinghours=4;;
-	*)
-	echo "employee is absent"
-	Workinghours=0;;
-esac
-salary=$(($Wageperhour*$Workinghours));
-Totalsalary=$(($salary+$Totalsalary));
+while [[ $Totalemphrs -le $maxhrsinamonth && $totalworkingdays -le $numworkingdays ]]
+do
+	((totalworkingdays++))
+	randomcheck=$((RANDOM%3))
+	case $randomcheck in
+		$Fulltime)
+		echo "employee is present"
+		Workinghours=8;;
+
+		$Parttime) 
+		echo "employee is Parttime"
+		Workinghours=4;;
+		*)
+		echo "employee is absent"
+		Workinghours=0;;
+	esac
+	Totalemphrs=$(($Totalemphrs+$Workinghours));
 done
+Totalsalary=$(($Totalemphrs*$Wageperhour));
+
 
