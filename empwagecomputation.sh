2bfaae20a2a2742fb62e678fcd9331616881f993
“Enter <yashwanth.k1119@gmail.com>
2023-06-06 14:34:06 +0530

    [yashwanth] Refactor: calculated employee's wage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to emoployee wage Computation"
 
+Wageperhour=20
+fulldayhour=8
 randomcheck=$((RANDOM%2))
-if [$randomcheck -eq 1 ]
+if [ $randomcheck -eq 1 ]
 then
 	echo "employee is present"
+	Workinghours=8
 else
 	echo "employee is absent"
+	Workinghours=0
 fi
+salary=$(($Wageperhour*$Workinghours))
+echo "salary of emp :" $salary
